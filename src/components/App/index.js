2050c0b0fe3e77c9c213968e5e2dfc53bb32f768
Carlos Vega Meyer <carlosvega09@gmail.com>
2017-06-07 07:51:37 -0300

    Fixes styling on web

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Header from '../../containers/Header'
 import MainSection from '../../containers/MainSection'
-import 'todomvc-app-css/index.css'
+import '../../index.css'
 
 const App = ({todos, actions}) => (
   <div>
